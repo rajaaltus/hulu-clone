e76690c877dffafddf6020d5fca3f984920c2717
rajaaltus <rajaaltus@gmail.com>
2021-08-08 11:56:56 +0700

    yts added

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -3,28 +3,25 @@ import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 
 const Thumbnail = forwardRef(({ result }) => {
-  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
     <div className="group p-2 cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         alt={result.title}
-        src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
-        }
+        src={result.medium_cover_image}
         layout="responsive"
-        height={1080}
-        width={1920}
+        height={345}
+        width={230}
+        className="object-cover"
       />
       <div className="p-2">
-        <p className="truncate max-w-md">{result.overview}</p>
+        <p className="truncate max-w-md">{result.description_full}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
-          {result.title || result.original_name}
+          {result.title || result.title_english}
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {result.media_type && `${result.media_type} · `}{" "}
-          {result.release_date || result.first_air_date} ·{" "}
-          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+          {result.torrents[0].quality && `${result.torrents[0].quality} · `}{" "}
+          {result.year || result.year} · <ThumbUpIcon className="h-5 mx-2" />{" "}
+          {result.rating}
         </p>
       </div>
     </div>
