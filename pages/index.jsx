b9faefd3d4557aca72a1f21567174d24061b2b7d
rajaaltus <rajaaltus@gmail.com>
2021-10-05 10:02:50 +0700

    fixed bugs

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,26 +5,22 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
-import { useRouter } from "next/router";
 
 export default function Home({ results }) {
   const [moreResults, setMoreResults] = useState(results);
-  const router = useRouter();
   useEffect(() => {
     setMoreResults(results);
   }, [results]);
-  console.log(router);
   return (
     <div>
       <Head>
         <title>Padam 2.0</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Padam for download" />
+        <meta name="description" content="torrent" />
       </Head>
-
       <Header />
-
       <Nav />
-
       <Results moreResults={moreResults} />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
         "3xl": "2000px"
+      },
+      colors: {
+        "back": "#06202A"
       }
     },
   },
